a16ddbf9d2d1f904accb458d8aa22e08370ccca9
Jola Rozylo <jolanta.rozylo@gmail.com>
2023-05-23 16:36:41 +0100

    Changed link in footer

--- src/Footer.js
+++ src/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className="Footer">
       <small>
-        <a href="https://github.com/JolaRozylo/my-humble-weather-app" target="_blank" rel="noreferrer">
+        <a href="https://github.com/JolaRozylo/react-weather-app" target="_blank" rel="noreferrer">
           Open-source code
         </a>{" "}
         by Jola Rozylo
