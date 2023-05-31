2212c269b4d368a78f9b4d3a039ec72a22458588
Jola Rozylo <jolanta.rozylo@gmail.com>
2023-05-31 13:18:21 +0100

    Added FontAwesome/styled icons

--- src/Search.js
+++ src/Search.js
@@ -1,9 +1,11 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { faLocationDot } from "@fortawesome/free-solid-svg-icons";
 
 export default function Search() {
   return (
     <div className="Wrapper">
-      <header>What's the weather like in...</header>
       <div className="row">
         <div className="col-8">
           <form className="SearchCity">
@@ -12,12 +14,14 @@ export default function Search() {
         </div>
         <div className="col-2">
           <button type="button" className="btn btn-light" id="btn-search">
-            <i className="fas fa-search icons"></i>
+            <i>
+              <FontAwesomeIcon icon={faMagnifyingGlass} className="FAicons" />
+            </i>
           </button>
         </div>
         <div className="col-2">
           <button type="button" className="btn btn-light" id="btn-mylocation">
-            <i className="fas fa-search-location icons"></i>
+            <FontAwesomeIcon icon={faLocationDot} className="FAicons" />
           </button>
         </div>
       </div>
